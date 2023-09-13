9c9d0d24c1ada8452cbcd90e3be239e7294de292
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-13 21:22:25 +0530

    added welcome

--- main.cs
+++ main.cs
@@ -1,3 +1,4 @@
-Console.WriteLine("Hello Karnisha")
- Console.WriteLine("Wc Karnisha") 
-Console.WriteLine(" Karnisha here")
+Console.WriteLine("Hello Karnisha");
+ Console.WriteLine("Wc Karnisha") ;
+Console.WriteLine(" Karnisha here");
+Console.WriteLine(" welcome");
